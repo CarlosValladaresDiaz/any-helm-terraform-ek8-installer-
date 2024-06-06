3ef7fe39a7f1f4d5eb8ee529af9a83e07b0fc0be
CarlosValladaresDiaz <carlos.diaz.creadoresti@gmail.com>
2024-06-05 21:12:35 -0600

    updatesd provider

--- provider.tf
+++ provider.tf
@@ -16,32 +16,4 @@ terraform {
   required_version = ">= 0.13.0"
   
 }
-
-provider "kubernetes" {
-  host     = "https://5F6CCBCB4A4E25132B1054A82A218324.gr7.us-east-1.eks.amazonaws.com"
-  client_certificate = base64decode(
-    yamldecode(civo_kubernetes_cluster.cluster.kubeconfig).users[0].user.client-certificate-data
-  )
-  client_key = base64decode(
-    yamldecode(civo_kubernetes_cluster.cluster.kubeconfig).users[0].user.client-key-data
-  )
-  cluster_ca_certificate = base64decode(
-    yamldecode(civo_kubernetes_cluster.cluster.kubeconfig).clusters[0].cluster.certificate-authority-data
-  )
-}
-
-provider "helm" {
-    kubernetes {
-    host     = data.civo_kubernetes_cluster.cluster.api_endpoint
-    client_certificate = base64decode(
-      yamldecode(civo_kubernetes_cluster.cluster.kubeconfig).users[0].user.client-certificate-data
-    )
-    client_key = base64decode(
-      yamldecode(civo_kubernetes_cluster.cluster.kubeconfig).users[0].user.client-key-data
-    )
-    cluster_ca_certificate = base64decode(
-      yamldecode(civo_kubernetes_cluster.cluster.kubeconfig).clusters[0].cluster.certificate-authority-data
-    )
-  }
-}
 */
